2bcc9ab1fbb8f23a1e18b565fbcf69ae6568bfb6
serkutakgul <56157543+serkutakgul@users.noreply.github.com>
2020-01-26 21:57:20 +0300

    Joke App Is Completed

---
+++ src/main/java/com/srkt/joke/springbootjokeapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.srkt.joke.springbootjokeapp.controllers;
+
+import com.srkt.joke.springbootjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService){
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/com/srkt/joke/springbootjokeapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.srkt.joke.springbootjokeapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
